1c0808bcdc7b539e39e1183d7586212e5b1b6500
heyyyelijah <elijahmamuri@gmail.com>
2022-09-18 16:58:21 +0800

    reverted original urls for static and media files

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -149,13 +149,13 @@ except:
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
